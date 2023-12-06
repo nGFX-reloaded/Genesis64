0974431f12bf56962b95940111edc13ed4badc9f
nGFX-reloaded <o.sons@drygoods.de>
2023-12-06 17:09:48 +0100

    Working on it ...

--- Genesis64/wwwroot/js/site.js
+++ Genesis64/wwwroot/js/site.js
@@ -1,5 +1,17 @@
 ﻿
 // Let's make a thing
-let g64 = Genesis64.Instance();
+let g64 = {};
 
-g64.Setup(document.getElementById("genesis64"));
+function Ready(fn) {
+	if (document.readyState !== "loading") {
+		fn();
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
+Ready(function () {
+	g64 = Genesis64.Instance;
+	g64.Setup(document.getElementById("genesis64"));
+	g64.Temp();
+});
