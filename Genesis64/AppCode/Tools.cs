02c66d85dab541fe19f733ea6bc909bf4424fbc3
nGFX-reloaded <o.sons@drygoods.de>
2023-12-06 17:07:56 +0100

    Text loader

---
+++ Genesis64/AppCode/Tools.cs
@@ -0,0 +1,18 @@
+﻿namespace Genesis64.AppCode {
+	public static class Tools {
+		public static string ReadFile(string filename) {
+			string strContent = "File not found: {filename}";
+
+			try {
+				using (StreamReader sr = new(filename)) {
+					strContent = sr.ReadToEnd();
+				}
+			} catch (Exception ex) {
+				//strContent = ex.Message;
+			}
+
+			return strContent;
+
+		}
+	}
+}
